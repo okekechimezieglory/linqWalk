226fd4a67f36e19f69469fb5fc22ae977afe7d63
chimexGitHub <okekechimezieglory@gmail.com>
2022-08-19 20:30:48 +0100

    Add project files.

---
+++ LinqWalkthrough/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqWalkthrough
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+
+            List<Employee> employees = new List<Employee>()
+            {
+                new Employee() {Id=1, Name="John"},
+                new Employee() {Id=2, Name="Bisi"}
+            };
+
+            IEnumerable<Employee> query = from emp in employees
+                                          where emp.Id == 1
+                                          select emp;
+
+            foreach (var q in query)
+            {
+                Console.WriteLine("Id = " + q.Id + " and name = " + q.Name);
+            }
+            //IEnumerable<int> querySyntax = from obj in list 
+            //                               where obj > 2 
+            //                               select obj;
+            Console.WriteLine("Hello World!");
+            //Console.WriteLine(querySyntax);
+        }
+
+        class Employee
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
